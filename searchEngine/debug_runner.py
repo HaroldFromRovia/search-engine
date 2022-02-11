5ae0a6fc36ec70ddd31e050c30287bece71c297a
Zagir Dingizbaev <zagir.dingizbaev@waveaccess.ru>
2022-02-11 19:55:10 +0300

    Remove redundant folders

---
+++ searchEngine/debug_runner.py
@@ -0,0 +1,2 @@
+from scrapy import cmdline
+cmdline.execute("scrapy runspider scrapper.py".split())
--- searchEngine/scrapper.py
+++ searchEngine/scrapper.py
@@ -4,33 +4,67 @@ import time
 import htmlmin
 import scrapy
 
-PAGES_COUNT = 99
-BASE_ITERATION = 30000
-PATH = '../resources'
+# To avoid getting banned by site for spamming. Value in seconds
+REQUEST_DELAY = 1
+
 BASE_URL = 'https://stackoverflow.com/questions/{}'
+DEFAULT_ENCODING = "UTF-8"
+
+PAGES_COUNT = 1
+BASE_ITERATION = 30000
+
+RESOURCE_PATH = '../resources'
+INDEX_FILE_NAME = "index.txt"
+PAGES_PATH = 'pages'
+INDEX_FILE_PATH = os.path.join(RESOURCE_PATH, INDEX_FILE_NAME)
+PAGES_PATH = os.path.join(RESOURCE_PATH, PAGES_PATH)
 
 
 def init_urls():
     urls = []
+
     for i in range(PAGES_COUNT):
-        urls.append(BASE_URL.format(i + BASE_ITERATION))
+        url = BASE_URL.format(i + BASE_ITERATION)
+        urls.append(url)
+
     return urls
+
+
+def init_folders():
+    if not os.path.exists(PAGES_PATH):
+        os.makedirs(PAGES_PATH, exist_ok=True)
+    if not os.path.isfile(INDEX_FILE_PATH):
+        with open(INDEX_FILE_PATH, 'w'): pass
+
+
+def append_to_index(index: str, url):
+    with open(INDEX_FILE_PATH, 'a') as index_file:
+        index_file.write(index + ' ' + url + '\n')
 
 
 class Spider(scrapy.Spider):
     name = "crawler"
 
-    def start_requests(self):
-        urls = init_urls()
+    def __init__(self):
+        super(Spider, self).__init__()
 
-        for url in urls:
+        self.urls = init_urls()
+        self.index_iteration = 0
+        init_folders()
+
+    def start_requests(self):
+        for url in self.urls:
             yield scrapy.Request(url=url, callback=self.parse)
-            time.sleep(3)
+            time.sleep(REQUEST_DELAY)
 
     def parse(self, response, **kwargs):
         content = response.xpath('//div[@id="mainbar"]').get()
-        minified_content = htmlmin.minify(content, remove_comments=True, remove_all_empty_space=True, reduce_empty_attributes=True)
+        minified_content = htmlmin.minify(content, remove_comments=True, remove_all_empty_space=True,
+                                          reduce_empty_attributes=True)
 
-        filename = response.url.split("/")[-1] + '.html'
-        with open(os.path.join(PATH, filename), 'w') as f:
+        filename = str(self.index_iteration) + '.html'
+        with open(os.path.join(PAGES_PATH, filename), 'w', encoding=DEFAULT_ENCODING) as f:
             f.write(minified_content)
+
+        append_to_index(str(self.index_iteration), response.url)
+        self.index_iteration += 1
