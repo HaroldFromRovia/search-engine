46c27b15792ca79fb406d32c85f28884e5804a88
Zagir Dingizbaev <zagir.dingizbaev@waveaccess.ru>
2022-02-13 15:01:12 +0300

    Remove redundant files. Rename file

--- crawler/debug_runner.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-from scrapy import cmdline
-
-if __name__ == '__main__':
-    cmdline.execute(sys.argv[1:])
--- crawler/scrapper.py
+++ crawler/scrapper.py
@@ -1,70 +1,24 @@
-import os
+import requests
 import time
 
-import htmlmin
-import scrapy
-
-# To avoid getting banned by site for spamming. Value in seconds
-REQUEST_DELAY = 1
-
-BASE_URL = 'https://stackoverflow.com/questions/{}'
-DEFAULT_ENCODING = "UTF-8"
-
-PAGES_COUNT = 100
+PAGES_COUNT = 99
 BASE_ITERATION = 30000
+PATH = '../html'
+BASE_URL = 'https://stackoverflow.com/questions/{}'
 
-RESOURCE_PATH = '../resources'
-INDEX_FILE_NAME = "index.txt"
-PAGES_PATH = 'pages'
-INDEX_FILE_PATH = os.path.join(RESOURCE_PATH, INDEX_FILE_NAME)
-PAGES_PATH = os.path.join(RESOURCE_PATH, PAGES_PATH)
+counter = 0
+if __name__ == '__main__':
+    file = open('index.txt', 'w')
+    for i in range(1000):
+        r = requests.get(BASE_URL.format(130000 + i), allow_redirects=True)
 
+        if r.status_code == 200:
+            open('../html/{}.html'.format(i), 'wb').write(r.content)
+            file.write('{id}.html https://stackoverflow.com/questions/{id}\n'.format(id=i))
+            print(i)
+            counter += 1
+        time.sleep(1)
+        if counter > 150:
+            break
 
-def init_urls():
-    urls = []
-
-    for i in range(PAGES_COUNT):
-        url = BASE_URL.format(i + BASE_ITERATION)
-        urls.append(url)
-
-    return urls
-
-
-def init_folders():
-    if not os.path.exists(PAGES_PATH):
-        os.makedirs(PAGES_PATH, exist_ok=True)
-    if not os.path.isfile(INDEX_FILE_PATH):
-        with open(INDEX_FILE_PATH, 'w'): pass
-
-
-def append_to_index(index: str, url):
-    with open(INDEX_FILE_PATH, 'a') as index_file:
-        index_file.write(index + ' ' + url + '\n')
-
-
-class Spider(scrapy.Spider):
-    name = "crawler"
-
-    def __init__(self):
-        super(Spider, self).__init__()
-
-        self.urls = init_urls()
-        self.index_iteration = 0
-        init_folders()
-
-    def start_requests(self):
-        for url in self.urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-            time.sleep(REQUEST_DELAY)
-
-    def parse(self, response, **kwargs):
-        content = response.xpath('//div[@id="mainbar"]').get()
-        minified_content = htmlmin.minify(content, remove_comments=True, remove_all_empty_space=True,
-                                          reduce_empty_attributes=True)
-
-        filename = str(self.index_iteration) + '.html'
-        with open(os.path.join(PAGES_PATH, filename), 'w', encoding=DEFAULT_ENCODING) as f:
-            f.write(minified_content)
-
-        append_to_index(str(self.index_iteration), response.url)
-        self.index_iteration += 1
+    file.close()
